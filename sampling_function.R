26338a5892696b776c5ab447722a6609399b90d3
Anna Halldorsdottir <alh323@cornell.edu>
2020-10-28 20:15:43 -0400

    sampling function, not quite finished yet

---
+++ sampling_function.R
@@ -0,0 +1,59 @@
+
+CYGNSSsampling <- function(y, type, n) {
+  
+  # y - object to sample (? x 4 [ws, lon, lat, time])
+  # type - string: 'random', or 'every-few', or 'averaging'
+  # n - if n > 1: size of subsample, else if 0 < n <= 1: the percentage 
+  #               of size of population to subsample
+  ## RETURNS: an nx4 matrix, a subsample of y.
+  
+  rows = dim(y)[1]
+  cols = dim(y)[2]
+  
+  #assert that the input y is valid
+  if (cols != 4) { 
+    print('ERROR: Make sure that the input y has 4 columns: ws, lon, lat, time')
+    return(NULL)
+  }
+  
+  #assert that the input n is valid
+  if (n < 0) { 
+    print('ERROR: Invalid number of samples.')
+    return(NULL)
+  } else if (n > len) {
+    print('ERROR: n is larger than the size of the population.')
+    return(NULL)
+  }
+  
+  #get the number of samples that are needed (Nsamples)
+  if (n <= 1) { #get the number of samples that are needed (Nsamples)
+    Nsamples = floor(n*len)
+  } else {
+    Nsamples = n
+  }
+  
+  
+  if (type == 'random') { # simple random sampling, without replacement.
+    
+    ind = sample(1:rows, Nsamples, replace=FALSE)
+    sampledResult = y[ind,]
+    return(sampledResult)
+    
+  } else if (type == 'every-few') { # only take every few* observations, *depending on Nsamples
+    
+    by = floor(rows/Nsamples)
+    ind = seq(1,rows,by)
+    sampledResult = y[ind,]
+    return(sampledResult)
+    
+  } else if (type == 'averaging') { # average every few* observations together, *depending on Nsamples
+    
+    # TO DO
+    
+  } else { # assert that input type is valid
+    print('ERROR: Invalid type.')
+    return(NULL)
+  }
+  
+  
+}
